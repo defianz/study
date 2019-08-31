e45e657756efe79aaccf567abebffcce83e9f09a
defian <iautm91@gmail.com>
2019-09-01 08:43:47 +0900

    2019-09-01_08-43-46

--- Coding/C++_Code_by_defian/src/Backjoon/삼성기출_낚시왕.cpp
+++ Coding/C++_Code_by_defian/src/Backjoon/삼성기출_낚시왕.cpp
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 #include <vector>
+#include <queue>
+#include <memory.h>
 using namespace std;
 
 struct shark{
@@ -10,14 +12,18 @@ struct shark{
 	int dir;
 	int vel;
 	int size;
+	int sindex;
 };
 
 
 int r, c, m, ans = 0;
-vector<shark> sharks(10001);
-
+vector<shark> sharks(10002);
+int tmp[101][101] = { 0, };
+int tmpindex;
 
 void moveshark(){
+
+	memset(tmp, 0, sizeof(tmp));
 
 	for (int i = 0; i < m; i++){
 
@@ -72,6 +78,24 @@ void moveshark(){
 					sharks[i].y = c - b;
 				}
 			}
+
+			int a = sharks[i].x;
+			int b = sharks[i].y;
+			int c = sharks[i].size;
+
+			if (c > tmp[a][b] && tmp[a][b] !=0){
+				sharks[tmpindex].size = 0;
+				tmp[a][b] = c;
+				tmpindex = sharks[i].sindex;
+			}
+			else if (c > tmp[a][b]){
+				tmp[a][b] = c;
+				tmpindex = sharks[i].sindex;
+			}
+			else {
+				sharks[i].size = 0;
+			}
+			
 		}
 	}
 }
@@ -84,13 +108,14 @@ int main(){
 
 	for (int i = 0; i < m; i++){
 		scanf("%d %d %d %d %d", &sharks[i].x, &sharks[i].y, &sharks[i].vel, &sharks[i].dir, &sharks[i].size);
+		sharks[i].sindex = i;
 		//printf("상어다 %d : %d %d %d %d %d \n", i, sharks[i].x, sharks[i].y, sharks[i].vel, sharks[i].dir, sharks[i].size);
 	}
 
 
 
 	for (int time = 1; time <= c; time++){
-		//printf("##### 시간은 : %d\n", time);
+		//printf("##### 시간은 : %d\n", ktime);
 
 
 		//# 어부가 잡음
@@ -105,31 +130,91 @@ int main(){
 		//printf("잡은상어는 : %d\n", index);
 		ans += sharks[index].size;
 		sharks[index].size = 0;
-
-
+//
+//		f/*or (int i = 0; i < m; i++){
+//			printf("잡은후 상어다 %d : %d %d %d %d %d \n", i, sharks[i].x, sharks[i].y, sharks[i].vel, sharks[i].dir, sharks[i].size);
+//		}
+//*/
 
 		//# 상어 이동
 
 		moveshark();
+	/*	for (int i = 0; i < m; i++){
+			printf("이동후 상어다 %d : %d %d %d %d %d \n", i, sharks[i].x, sharks[i].y, sharks[i].vel, sharks[i].dir, sharks[i].size);
+		}*/
 
 		//# 상어 잡아먹힘
 
-		int map[101][101] = { 0, };
-		int tmp[101][101] = { 100001, };
-		
-		for (int i = 0; i < m; i++){
-			if (sharks[i].size != 0){
-				if (map[sharks[i].x][sharks[i].y] < sharks[i].size){
-					map[sharks[i].x][sharks[i].y] = sharks[i].size;
-					sharks[tmp[sharks[i].x][sharks[i].y]].size = 0;
-					tmp[sharks[i].x][sharks[i].y] = i;
-				}
-				else if (map[sharks[i].x][sharks[i].y] > sharks[i].size) {
-					sharks[i].size = 0;
-				}
-			}
-		}
+		//queue<shark> q[101][101];
+		//for (int i = 0; i < m; i++){
+		//	if (sharks[i].size != 0){
+		//		q[sharks[i].x][sharks[i].y].push(sharks[i]);
+		//		//printf("q[a][b] 사이즈는 : %d %d %d", sharks[i].x, sharks[i].y, q[sharks[i].x][sharks[i].y].size());
+		//	}
+		//}
 
+		//for (int a = 0; a < 101; a++){
+		//	for (int b = 0; b < 101; b++){
+		//		if (q[a][b].size() >1){
+		//			int maxsize = 0;
+		//			while (q[a][b].size() != 1){
+		//				shark tsha = q[a][b].front();
+		//				q[a][b].pop();
+		//				if (maxsize <= tsha.size){
+		//					maxsize = tsha.size;
+		//					q[a][b].push(tsha);
+		//				}
+		//				else {
+		//					int catindex = tsha.sindex;
+		//					sharks[catindex].size = 0;
+		//				}
+		//			}
+		//		}
+		//	}
+		//}
+
+
+
+		//int map[101][101];
+
+		//for (int a = 0; a < 101; a++){
+		//	for (int b = 0; b < 101; b++){
+		//		map[a][b] = 0;
+		//	}
+		//}
+
+
+		//int tmp[101][101];
+
+		//for (int a = 0; a < 101; a++){
+		//	for (int b = 0; b < 101; b++){
+		//		tmp[a][b]  = 10001;
+		//	}
+		//}
+		//
+		//
+		//for (int i = 0; i < m; i++){
+		//	if (sharks[i].size != 0){
+		//		if (map[sharks[i].x][sharks[i].y] < sharks[i].size){
+		//			map[sharks[i].x][sharks[i].y] = sharks[i].size;
+		//			sharks[tmp[sharks[i].x][sharks[i].y]].size = 0;
+		//			//printf("1로 와서 x,y는 : %d, %d\n", sharks[i].x,sharks[i].y);/*
+		//			//printf("1로 와서 tmp[sharks[i].x][sharks[i].y] : %d\n", tmp[sharks[i].x][sharks[i].*/y]);
+		//			tmp[sharks[i].x][sharks[i].y] = i;
+
+		//		}
+		//		else if (map[sharks[i].x][sharks[i].y] > sharks[i].size) {
+		//			sharks[i].size = 0;
+		//			//printf("일로오나2222");
+		//		}
+		//	}
+		//}
+
+	/*	for (int i = 0; i < m; i++){
+			printf("잡아먹힌후 상어다 %d : %d %d %d %d %d \n", i, sharks[i].x, sharks[i].y, sharks[i].vel, sharks[i].dir, sharks[i].size);
+
+		}
+*/
 
 
 
@@ -150,12 +235,12 @@ int main(){
 
 
 
-
-	/*	for (int i = 0; i < m; i++){
+/*
+		for (int i = 0; i < m; i++){
 			printf("상어다 %d : %d %d %d %d %d \n", i, sharks[i].x, sharks[i].y, sharks[i].vel, sharks[i].dir, sharks[i].size);
 		}
-		printf("사이즈는 : %d\n", ans);
-*/
+		printf("답 사이즈는 : %d\n", ans);*/
+
 	}
 
 	printf("%d", ans);
