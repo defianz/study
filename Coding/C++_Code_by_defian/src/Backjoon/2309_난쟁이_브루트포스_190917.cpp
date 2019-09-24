51ee0ee12cf9f6adc8b136cdbaf7ff0e957b4292
defian <iautm91@gmail.com>
2019-09-24 21:28:20 +0900

    2019-09-24_21-28-14

---
+++ Coding/C++_Code_by_defian/src/Backjoon/2309_난쟁이_브루트포스_190917.cpp
@@ -0,0 +1,34 @@
+
+
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int a[9];
+int n = 9;
+
+int main(){
+
+	int sum = 0;
+	for (int i = 0; i < n; i++){
+		scanf("%d", &a[i]);
+		sum += a[i];
+	}
+
+	sort(a, a + n);
+
+	for (int i = 0; i < n; i++){
+		for (int j = i+1; j < n; j++){
+			if (sum - a[i] - a[j] == 100){
+				for (int k = 0; k < n; k++){
+					if (i == k || j == k){
+						continue;
+					}
+					printf("%d\n", a[k]);
+				}
+				return 0;
+			}
+		}
+	}
+
+}
