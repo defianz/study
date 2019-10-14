4f9c2de1ea5a2bc75b2921a910c339fc0f0b94b6
김형철 <iautm91@gmail.com>
2019-10-15 05:37:36 +0900

    2019-10-15_05-37-33

--- Coding/C++_Code_by_defian/src/Backjoon/14891_톱니바퀴_삼성기출_191013.cpp
+++ Coding/C++_Code_by_defian/src/Backjoon/14891_톱니바퀴_삼성기출_191013.cpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include <queue>
 #include <cstring>
+#include <math.h>
 using namespace std;
 
 int top[5][8];
@@ -21,47 +22,67 @@ void rotate(int ctop, int cdir){
 	else{
 		visited[ctop] = true;
 		if (cdir == -1){
-			p[ctop] = (p[ctop] + 1) % 8;
 			if (ctop == 2 || ctop == 3){
+				bool flag1 = false;
+				bool flag2 = false;
 				if (top[ctop][(p[ctop] + 2) % 8] != top[ctop + 1][(p[ctop + 1] + 6) % 8]){
-					rotate(ctop + 1, 1);
+					flag1 = true;
 				}
 				if (top[ctop][(p[ctop] + 6) % 8] != top[ctop - 1][(p[ctop - 1] + 2) % 8]){
-					rotate(ctop - 1, 1);
+					flag2 = true;
 				}
+				p[ctop] = (p[ctop] + 1) % 8;
+				if (flag1) rotate(ctop + 1, 1);
+				if (flag2) rotate(ctop - 1, 1);
 			}
 			else if (ctop == 1){
+				bool flag3 = false;
 				if (top[1][(p[1] + 2) % 8] != top[2][(p[2] + 6) % 8]){
-					rotate(2, 1);
+					flag3 = true;
 				}
+				p[ctop] = (p[ctop] + 1) % 8;
+				if (flag3) rotate(2, 1);
+		
 			}
 			else {
+				bool flag4 = false;
 				if (top[4][(p[4] + 6) % 8] != top[3][(p[3] + 2) % 8]){
-					rotate(3, 1);
+					flag4 = true;
 				}
+				p[ctop] = (p[ctop] + 1) % 8;
+				if (flag4) rotate(3, 1);
 			}
 		}
 		else {
-			p[ctop] = (p[ctop] + 7) % 8;
-			if (ctop == 2 || ctop == 3){
+			if (ctop == 2 || ctop == 3){ 
+				bool flag1 = false;
+				bool flag2 = false;
 				if (top[ctop][(p[ctop] + 2) % 8] != top[ctop + 1][(p[ctop + 1] + 6) % 8]){
-					rotate(ctop + 1, -1);
+					flag1 = true;
+					
 				}
 				if (top[ctop][(p[ctop] + 6) % 8] != top[ctop - 1][(p[ctop - 1] + 2) % 8]){
-					rotate(ctop - 1, -1);
+					flag2 = true;					
 				}
+				p[ctop] = (p[ctop] + 7) % 8;
+				if (flag1) rotate(ctop + 1, -1);
+				if (flag2) rotate(ctop - 1, -1);
 			}
 			else if (ctop == 1){
-				printf("포인터 ctop은 %d\n", p[ctop]);
-				printf("값이 뭔데 %d %d 포인터는 %d\n", top[ctop][(p[ctop] + 2) % 8], top[ctop + 1][(p[ctop + 1] + 6) % 8], p[ctop + 1]);
-				if (top[1][(p[1] + 2) % 8] != top[2][(p[2] + 6) % 8]){
-					rotate(2, -1);
+				bool flag3 = false;
+				if (top[1][(p[1] + 2) % 8] != top[2][(p[2] + 6) % 8]){	
+					flag3 = true;
 				}
+				p[ctop] = (p[ctop] + 7) % 8;
+				if (flag3) rotate(2, -1);
 			}
 			else {
-				if (top[ctop][(p[ctop] + 6) % 8] != top[ctop - 1][(p[ctop - 1] + 2) % 8]){
-					rotate(3, -1);
+				bool flag4 = false;
+				if (top[4][(p[4] + 6) % 8] != top[3][(p[3] + 2) % 8]){
+					flag4 = true;
 				}
+				p[ctop] = (p[ctop] + 7) % 8;
+				if (flag4) rotate(3, -1);
 			}
 		}
 		return;
@@ -89,24 +110,27 @@ int main(){
 	for (int i = 0; i < k; i++){
 		int ctop = m[i].first;
 		int cdir = m[i].second;
-		fill(visited + 1, visited + 5, false);
+		memset(visited, false, sizeof(visited));
 		// 명령어별 초기화
 
 		rotate(ctop, cdir);
+
+
+		//printf("%d번째 결과값 디버깅 \n",i);
+		//for (int i = 1; i <= 4; i++){
+		//	//printf("%d \n", p[i]);
+		//	for (int j = p[i]; j < p[i] + 8; j++){
+		//		printf("%d ", top[i][j % 8]);
+		//	}
+		//	printf("\n");
+		//}
+		//printf("\n");
+
 	}
 
 	// 연산 완료
-	for (int i = 1; i <= 4; i++){
-		printf("%d ",p[i]);
-	}
-	printf("\n");
 
-	// 연산 완료
-	for (int i = 1; i <= 4; i++){
-		printf("%d ", top[i][p[i]]);
-	}
-	printf("\n");
-
+	
 
 	// 결과 계산
 	for (int i = 1; i <= 4; i++){
