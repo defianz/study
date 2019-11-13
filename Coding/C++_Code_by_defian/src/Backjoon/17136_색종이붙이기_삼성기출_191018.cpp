4d03827a4c999deea390620411e0b3ff4abb8b71
김형철 <iautm91@gmail.com>
2019-11-13 18:25:08 +0900

    2019-11-13_18-25-05

--- Coding/C++_Code_by_defian/src/Backjoon/17136_색종이붙이기_삼성기출_191018.cpp
+++ Coding/C++_Code_by_defian/src/Backjoon/17136_색종이붙이기_삼성기출_191018.cpp
@@ -2,62 +2,33 @@
 
 
 #include <stdio.h>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-
+int jong[6] = { 5, 5, 5, 5, 5 };
 int maps[11][11];
-vector<pair<int, int> > v;
-bool visited[11][11];
-int ans;
+int ans = -1;
 
-
-void bp(int index, int type,int cnt){
-	if (index > v.size()) {
-		ans = min(cnt, ans);
-		return;
-	}
-	if (visited[v[index].first][v[index].second]) bp(index + 1, type, cnt);
-	// 다음으로 넘어가기
-
-	// 그리기
-	int r = v[index].first;
-	int c = v[index].second;
-	if (type == 1){
-		visited[r][c] == true;
-	}
-	else if (type == 2){
-		for (int i = r; i <= r + 1; i++){
-			for (int j = c; j <= c + 1; j++){
-				if (maps[i][j] != 1) return;
-				visited[i][j] = true;
-			}
-		}
-	}
-
+void bp(int r, int c){
 
 }
 
-
-
 int main(){
 
-	
 	for (int i = 1; i <= 10; i++){
 		for (int j = 1; j <= 10; j++){
 			scanf("%d", &maps[i][j]);
+		}
+	}
+
+	for (int i = 1; i <= 10; i++){
+		for (int j = 1; j <= 10; j++){
 			if (maps[i][j] == 1){
-				v.push_back({ i, j });
+				bp(i, j);
 			}
 		}
 	}
 
-	for (int i = 1; i <= 5; i++){
-		bp(0,i);
-	}
-	
 
 
+	// 답 출력
+	printf("%d", ans);
 	return 0;
 }
