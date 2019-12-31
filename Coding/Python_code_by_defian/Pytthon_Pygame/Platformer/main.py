9c3643104538b5e126434b410a40025c1f741f72
김형철 <iautm91@gmail.com>
2019-12-31 14:24:47 +0900

    2019-12-31_14-24-45

--- Coding/Python_code_by_defian/Pytthon_Pygame/Platformer/main.py
+++ Coding/Python_code_by_defian/Pytthon_Pygame/Platformer/main.py
@@ -35,22 +35,36 @@ class Game:
         # load spritesheet image
         self.spritesheet = Spritesheet(path.join(img_dir, SPRITESHEET))
 
+        # cloud images
+        self.cloud_images = []
+        for i in range(1, 4):
+            self.cloud_images.append(pg.image.load(
+                path.join(img_dir, 'cloud{}.png'.format(i))).convert())
+
         # load sounds
         self.snd_dir = path.join(self.dir, 'snd')
         self.jump_sound = pg.mixer.Sound(path.join(self.snd_dir, 'Jump33.wav'))
+        self.boost_sound = pg.mixer.Sound(
+            path.join(self.snd_dir, 'Boost16.wav'))
 
     def new(self):
         # start a new gamezmfhtmg
         self.score = 0
-        self.all_sprite = pg.sprite.Group()
+        self.all_sprite = pg.sprite.LayeredUpdates()
         self.platforms = pg.sprite.Group()
+        self.powerups = pg.sprite.Group()
+        self.mobs = pg.sprite.Group()
+        self.clouds = pg.sprite.Group()
         self.player = Player(self)
-        self.all_sprite.add(self.player)
+
         for plat in PLATFORM_LIST:
-            p = Platform(self, *plat)
-            self.all_sprite.add(p)
-            self.platforms.add(p)
+            Platform(self, *plat)
+
+        self.mob_timer = 0
         pg.mixer.music.load(path.join(self.snd_dir, 'Happy Tune.ogg'))
+        for i in range(8):
+            c = Cloud(self)
+            c.rect.y += 500
         self.run()
 
     def run(self):
@@ -68,27 +82,57 @@ class Game:
         # Game Loop - Update
         self.all_sprite.update()
 
+        # spawan a mob?
+        now = pg.time.get_ticks()
+        if now - self.mob_timer > 5000 + random.choice([-1000, -500, 0, 500, 1000]):
+            self.mob_timer = now
+            Mob(self)
+
+        # hit mobs?
+        mob_hits = pg.sprite.spritecollide(
+            self.player, self.mobs, False, pg.sprite.collide_mask)
+        if mob_hits:
+            self.playing = False
+
         # check if player hits a platform - only if boarding
         if self.player.vel.y > 0:
             hits = pg.sprite.spritecollide(self.player, self.platforms, False)
             if hits:
                 lowest = hits[0]
                 for hit in hits:
-                    if hit.rect.bottom > lowest.rect.centery:
+                    if hit.rect.bottom > lowest.rect.bottom:
                         lowest = hit
-                if self.player.pos.y < lowest.rect.bottom:
-                    self.player.pos.y = lowest.rect.top
-                    self.player.vel.y = 0
-                    self.player.jumping = False
+                if self.player.pos.x < lowest.rect.right \
+                        and self.player.pos.x > lowest.rect.left:
+                    if self.player.pos.y < lowest.rect.centery:
+                        self.player.pos.y = lowest.rect.top
+                        self.player.vel.y = 0
+                        self.player.jumping = False
 
         # if player reaches top 1/4 of screen
         if self.player.rect.top <= HEIGHT / 4:
+            if random.randrange(100) < 15:
+                Cloud(self)
             self.player.pos.y += max(abs(self.player.vel.y), 2)
+
+            for cloud in self.clouds:
+                cloud.rect.y += max(abs(self.player.vel.y/2), 2)
+            for mob in self.mobs:
+                mob.rect.y += max(abs(self.player.vel.y), 2)
+
             for plat in self.platforms:
                 plat.rect.y += max(abs(self.player.vel.y), 2)
                 if plat.rect.top >= HEIGHT:
                     plat.kill()
                     self.score += 10
+
+        # if player hits powerups
+        pow_hits = pg.sprite.spritecollide(self.player, self.powerups, True)
+        for pow in pow_hits:
+            if pow.type == 'boost':
+                self.boost_sound.play()
+                self.player.vel.y = -BOOST_POWER
+                self.player.jumping = False
 
         # Die!
         if self.player.rect.bottom > HEIGHT:
@@ -102,10 +146,8 @@ class Game:
         # 새로운 플랫폼 생성
         while len(self.platforms) < 6:
             width = random.randrange(50, 100)
-            p = Platform(self, random.randrange(0, WIDTH-width),
-                         random.randrange(-75, -30))
-            self.platforms.add(p)
-            self.all_sprite.add(p)
+            Platform(self, random.randrange(0, WIDTH-width),
+                     random.randrange(-75, -30))
 
     def events(self):
         # Game loop - events
@@ -126,7 +168,8 @@ class Game:
         # Game Loop - draw
         self.screen.fill(BGCOLOR)
         self.all_sprite.draw(self.screen)
-        self.screen.blit(self.player.image, self.player.rect)
+        # self.screen.blit(self.player.image, self.player.rect)
+        # self.screen.blit(self.player.image, self.player.rect)
         self.draw_text(str(self.score), 22, WHITE, WIDTH/2, 15)
         # *after* drawing everything , flip the display
         pg.display.flip()
@@ -134,7 +177,7 @@ class Game:
     def show_start_screen(self):
         # game splash / start screen
         pg.mixer.music.load(path.join(self.snd_dir, 'Yippee.ogg'))
-        pg.mixer.music
+        pg.mixer.music.play(loops=-1)
         self.screen.fill(BGCOLOR)
         self.draw_text(TITLE, 48, WHITE, WIDTH/2, HEIGHT/4)
         self.draw_text("Arrow to move, Space to jump",
@@ -144,11 +187,14 @@ class Game:
                        22, WHITE, WIDTH/2, 15)
         pg.display.flip()
         self.wait_for_key()
+        pg.mixer.music.fadeout(500)
 
     def show_go_screen(self):
         # game over/continue
         if not self.running:
             return
+        pg.mixer.music.load(path.join(self.snd_dir, 'Yippee.ogg'))
+        pg.mixer.music.play(loops=-1)
         self.screen.fill(BGCOLOR)
         self.draw_text("GAME OVER", 48, WHITE, WIDTH/2, HEIGHT/4)
         self.draw_text("Score: " + str(self.score),
@@ -165,6 +211,7 @@ class Game:
                            22, WHITE, WIDTH/2, HEIGHT/2+40)
         pg.display.flip()
         self.wait_for_key()
+        pg.mixer.music.fadeout(500)
 
     def wait_for_key(self):
         waiting = True
--- Coding/Python_code_by_defian/Pytthon_Pygame/Platformer/sprite.py
+++ Coding/Python_code_by_defian/Pytthon_Pygame/Platformer/sprite.py
@@ -3,7 +3,7 @@
 
 import pygame as pg
 from settings import *
-from random import choice
+from random import choice, randrange
 vec = pg.math.Vector2
 
 
@@ -22,7 +22,9 @@ class Spritesheet:
 
 class Player(pg.sprite.Sprite):
     def __init__(self, game):
-        pg.sprite.Sprite.__init__(self)
+        self._layer = PLAYER_LAYER
+        self.groups = game.all_sprite
+        pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
         self.walking = False
         self.jumping = False
@@ -125,17 +127,98 @@ class Player(pg.sprite.Sprite):
                 self.image = self.standing_frames[self.current_frame]
                 self.rect = self.image.get_rect()
                 self.rect.bottom = bottom
+        self.mask = pg.mask.from_surface(self.image)
+
+
+class Cloud(pg.sprite.Sprite):
+    def __init__(self, game):
+        self._layer = COLUD_LAYER
+        self.groups = game.all_sprite, game.clouds
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image = choice(self.game.cloud_images)
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        scale = randrange(50, 101)/100
+        self.image = pg.transform.scale(self.image, (int(
+            self.rect.width * scale), int(self.rect.height*scale)))
+        self.rect.x = randrange(WIDTH - self.rect.width)
+        self.rect.y = randrange(-500, -50)
+
+    def update(self):
+        if self.rect.top > HEIGHT:
+            self.kill()
 
 
 class Platform(pg.sprite.Sprite):
     def __init__(self, game, x, y):
-        pg.sprite.Sprite.__init__(self)
+        self._layer = PLATFORM_LAYER
+        self.groups = game.all_sprite, game.platforms
+        pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
         images = [self.game.spritesheet.get_image(0, 288, 380, 94),
                   self.game.spritesheet.get_image(213, 1662, 201, 100)]
         self.image = choice(images)
         self.image.set_colorkey(BLACK)
-
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        if randrange(100) < POW_SPAWN_PCT:
+            Pow(self.game, self)
+
+
+class Pow(pg.sprite.Sprite):
+    def __init__(self, game, plat):
+        self._layer = POW_LAYER
+        self.groups = game.all_sprite, game.powerups
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.plat = plat
+        self.type = choice(['boost'])
+        self.image = self.game.spritesheet.get_image(820, 1805, 71, 70)
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = self.plat.rect.centerx
+        self.rect.bottom = self.plat.rect.top - 5
+
+    def update(self):
+        self.rect.bottom = self.plat.rect.top - 5
+        if not self.game.platforms.has(self.plat):
+            self.kill()
+
+
+class Mob(pg.sprite.Sprite):
+    def __init__(self, game):
+        self.groups = game.all_sprite, game.mobs
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.image_up = self.game.spritesheet.get_image(566, 510, 122, 139)
+        self.image_up.set_colorkey(BLACK)
+        self.image_down = self.game.spritesheet.get_image(568, 1534, 122, 135)
+        self.image_down.set_colorkey(BLACK)
+        self.image = self.image_up
+        self.rect = self.image.get_rect()
+        self.mask = pg.mask.from_surface(self.image)
+        self.rect.centerx = choice([-100, WIDTH + 100])
+        self.vx = randrange(1, 4)
+        if self.rect.centerx > WIDTH:
+            self.vx *= -1
+        self.rect.y = randrange(HEIGHT/2)
+        self.vy = 0
+        self.dy = 0.5
+
+    def update(self):
+        self.rect.x += self.vx
+        self.vy += self.dy
+        if self.vy > 3 or self.vy < -3:
+            self.dy *= -1
+        center = self.rect.center
+        if self.dy < 0:
+            self.image = self.image_up
+        else:
+            self.image = self.image_down
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.rect.y += self.vy
+        if self.rect.left > WIDTH + 100 or self.rect.right < -100:
+            self.kill()
