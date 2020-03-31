38507def3108ebe70b7f7dea1acd3029128c2918
김형철 <iautm91@gmail.com>
2020-03-31 19:33:14 +0900

    2020-03-31_19-33-13

---
+++ Coding/inflearn_JAVA/src/inflearn_JAVA/test_main.java
@@ -0,0 +1,76 @@
+package inflearn_JAVA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class test_main {
+	
+//	public int plus(int a, int b) {
+//		int res;
+//		res = a+ b;
+//		
+//		return res;
+//	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		SYSTEM.OUT.PRINTLN(APPLICANT.PLSS(10,5));
+//		
+//		SYSTEM.OUT.PRINTF("%S","HELLO WORLD\N");
+//		SYSTEM.OUT.PRINTLN("HELLO WORLD");
+//		SYSTEM.OUT.PRINTF("%D", 100%3);
+//		int[] a = null;
+//		System.out.println(a);
+//		a= new int[5];
+//		System.out.println(a);
+//		System.out.println(a[0]);
+//		a[0]=5;
+//		System.out.println(a[0]);
+//		int a =5;
+//		int b= 5;
+//		String str1 = new String("hi");
+//		String str2= new String("hi");
+//		System.out.println(a==b);
+//		
+//		System.out.println(str1==str2);
+//		System.out.println(str1.equals(str2));
+		
+//		testVO c1 = new testVO();
+//		c1.va = 100;
+//		System.out.println(c1.va);
+//		
+//		c1.method1();
+//		c1.method2();
+//		
+//		int[] c2 = new int[5];
+		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			int kor1,kor2,kor3;
+			int eng1,eng2,eng3;
+			int mat1,mat2,mat3;
+			
+			System.out.println();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+//		
+	}
+
+}
+
+//class Applicant {
+//	int a,b;
+//
+//	
+//	public static int plss(int a, int b) {
+//		int res;
+//		res = a + b;
+//		
+//		return res;
+//	}
+//}
