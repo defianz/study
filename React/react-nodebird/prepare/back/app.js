977c51d6405b219e79af3fe61ec4989a9b1f6930
Defian <iautm91@gmail.com>
2020-09-01 18:38:23 +0900

    React Study

--- React/react-nodebird/prepare/back/app.js
+++ React/react-nodebird/prepare/back/app.js
@@ -31,16 +31,22 @@ if (process.env.NODE_ENV === "production") {
   app.use(morgan("combined"));
   app.use(hpp());
   app.use(helmet());
+  app.use(
+    cors({
+      origin: "http://test-defian.tk",
+      credentials: true, // 쿠키 전달 옵션 : 전달하려면 true (default : false)
+    })
+  );
 } else {
   app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true, // 쿠키 전달 옵션 : 전달하려면 true (default : false)
+    })
+  );
 }
 
-app.use(
-  cors({
-    origin: ["http://localhost:3060", "nodebird.com", "http://15.164.98.68"],
-    credentials: true, // 쿠키 전달 옵션 : 전달하려면 true (default : false)
-  })
-);
 app.use("/", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -50,6 +56,11 @@ app.use(
     saveUninitialized: false,
     resave: false,
     secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      domain: process.env.NODE_ENV === "production" && ".test-defian.tk",
+    },
   })
 );
 app.use(passport.initialize());
