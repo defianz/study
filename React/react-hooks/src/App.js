5b8dc77e471bf7bbd464b368cf40687b325a5016
Defian <iautm91@gmail.com>
2020-09-08 16:16:30 +0900

    React Hooks Study

---
+++ React/react-hooks/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+
+  return { value, onChange };
+};
+
+const App = () => {
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="what is you name?" {...name} />
+    </div>
+  );
+};
+
+export default App;
