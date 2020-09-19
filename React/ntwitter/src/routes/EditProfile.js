c446d17dabae8c59df1b9fd45698445cfec58871
Defian <iautm91@gmail.com>
2020-09-19 13:37:40 +0900

    study

--- React/ntwitter/src/routes/EditProfile.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-// export default () => <Span>Edit Profile</Span>;
-const EditProfile = () => <span>Edit Profile</span>;
-
-export default EditProfile;
--- React/ntwitter/src/routes/Home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-// export default () => <Span>Home</Span>;
-
-const Home = () => <span>Home</span>;
-
-export default Home;
